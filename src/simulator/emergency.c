c5950ba2ccac639666a145b68ab76254ae6932ec
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2023-12-27 11:19:49 +0100

    Add command to exit emergency state.

--- src/simulator/emergency.c
+++ src/simulator/emergency.c
@@ -24,9 +24,16 @@
 /*** EMERGENCY local structures ***/
 
 /*******************************************************************/
+typedef enum {
+	EMERGENCY_STATE_IDLE = 0,
+	EMERGENCY_STATE_EMERGENCY,
+	EMERGENCY_STATE_LAST
+} EMERGENCY_state_t;
+
+/*******************************************************************/
 typedef struct {
 	SW2_context_t bpurg;
-	uint8_t previous_state;
+	EMERGENCY_state_t state;
 } EMERGENCY_context_t;
 
 /*** EMERGENCY external global variables ***/
@@ -44,7 +51,7 @@ void EMERGENCY_init(void) {
 	// Init GPIO.
 	SW2_init(&emergency_ctx.bpurg, &GPIO_BPURG, 0, 100); // EMERGENCY active low.
 	// Init context.
-	emergency_ctx.previous_state = 0;
+	emergency_ctx.state = EMERGENCY_STATE_IDLE;
 	lsmcu_ctx.emergency = 0;
 }
 
@@ -61,20 +68,34 @@ void EMERGENCY_process(void) {
 	if (emergency_ctx.bpurg.state == SW2_ON) {
 		EMERGENCY_trigger();
 	}
-	// Check global flag.
-	if ((lsmcu_ctx.emergency != 0) && (emergency_ctx.previous_state == 0)) {
-		// Trigger emergency brake.
-		MANOMETER_set_pressure(lsmcu_ctx.manometer_cg, 0, EMERGENCY_CG_SPEED_MBAR_PER_SECOND);
-		MANOMETER_set_pressure(lsmcu_ctx.manometer_re, 0, EMERGENCY_RE_SPEED_MBAR_PER_SECOND);
-		MANOMETER_set_pressure(lsmcu_ctx.manometer_cf1, ((lsmcu_ctx.manometer_cf1) -> pressure_limit_mbar), EMERGENCY_CF_SPEED_MBAR_PER_SECOND);
-		MANOMETER_set_pressure(lsmcu_ctx.manometer_cf2, ((lsmcu_ctx.manometer_cf2) -> pressure_limit_mbar), EMERGENCY_CF_SPEED_MBAR_PER_SECOND);
-		// Send sound command.
-		LSAGIU_write(LSMCU_OUT_EMERGENCY);
+	switch (emergency_ctx.state) {
+	case EMERGENCY_STATE_IDLE:
+		// Check flag.
+		if (lsmcu_ctx.emergency != 0) {
+			// Trigger emergency brake.
+			MANOMETER_set_pressure(lsmcu_ctx.manometer_cg, 0, EMERGENCY_CG_SPEED_MBAR_PER_SECOND);
+			MANOMETER_set_pressure(lsmcu_ctx.manometer_re, 0, EMERGENCY_RE_SPEED_MBAR_PER_SECOND);
+			MANOMETER_set_pressure(lsmcu_ctx.manometer_cf1, ((lsmcu_ctx.manometer_cf1) -> pressure_limit_mbar), EMERGENCY_CF_SPEED_MBAR_PER_SECOND);
+			MANOMETER_set_pressure(lsmcu_ctx.manometer_cf2, ((lsmcu_ctx.manometer_cf2) -> pressure_limit_mbar), EMERGENCY_CF_SPEED_MBAR_PER_SECOND);
+			// Send sound command.
+			LSAGIU_write(LSMCU_OUT_EMERGENCY_ON);
+			// Update state.
+			emergency_ctx.state = EMERGENCY_STATE_EMERGENCY;
+		}
+		break;
+	case EMERGENCY_STATE_EMERGENCY:
+		// Release emergency state only when BPURG is released and train is stopped.
+		if ((emergency_ctx.bpurg.state == SW2_OFF) && (lsmcu_ctx.speed_kmh == 0)) {
+			// Release emergency state.
+			lsmcu_ctx.emergency = 0;
+			// Send sound command.
+			LSAGIU_write(LSMCU_OUT_EMERGENCY_OFF);
+			// Update state.
+			emergency_ctx.state = EMERGENCY_STATE_IDLE;
+		}
+		break;
+	default:
+		// Unknown state.
+		break;
 	}
-	// Release emergency state only when train is stopped.
-	if ((emergency_ctx.bpurg.state == SW2_OFF) && (lsmcu_ctx.speed_kmh == 0)) {
-		lsmcu_ctx.emergency = 0;
-	}
-	// Update state.
-	emergency_ctx.previous_state = lsmcu_ctx.emergency;
 }
